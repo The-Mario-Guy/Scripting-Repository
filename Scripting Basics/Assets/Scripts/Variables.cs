d5482f6215d7642671bdd13c83ae5b958681c0ba
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-10-03 12:02:33 -0700

    Notes on Variables

---
+++ Scripting Basics/Assets/Scripts/Variables.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //Public Variables/Global Variables
+    public Variables cheese;
+    public float speed = 20;
+    public int score;
+    public string playerName = "Stanley";
+    public bool _isPlayerActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
