bc6e8b8cd6c7ce38add0491c45f23ea02e6d22f6
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-10-04 11:36:47 -0700

    Update Variables.cs

--- Scripting Basics/Assets/Scripts/Variables.cs
+++ Scripting Basics/Assets/Scripts/Variables.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Declaring Variables
+
     //Public Variables/Global Variables
     public Variables cheese;
     public float speed = 20;
     public int score;
     public string playerName = "Stanley";
     public bool _isPlayerActive = true;
+
+    //Private Member/Goal Variables
+    private float _maxSpeed = 100;
+    private int _totalScore = 0;
+    private string _username = "Parable";
+    private bool _isGameOver = false;  
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +27,12 @@ public class Variables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //declaring local variables
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+    private void UpdateScore(int amount)
+    {
+       //using a local variable to update the total score
+       _totalScore += amount;
     }
 }
