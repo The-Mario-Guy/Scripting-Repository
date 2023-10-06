d240f24e070a73a1b29dcf71a7fe1eb0a48a2c4d
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2023-10-06 12:38:11 -0700

    WE

---
+++ Scripting Basics/Assets/Scripts/Testing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    //Public Variables
+    public string firstName = "Guillermo";
+    public int gradeLevel = 12;
+    public float GPA = 3.66f;
+    public bool usingNotes = true;
+
+    //Private Variables
+    private int marioBros = 1;
+    private string favMovie = "The Super Mario Bros. Movie";
+    private float gasPrice = 6.09f;
+    private bool wasHelpful = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
